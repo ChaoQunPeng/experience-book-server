6a71e388e0f7d2e08ce413902b6319a2d37627bc
PengChaoQun <1152684231@qq.com>
2024-02-08 20:20:04 +0800

    ✨ feat: 增加环境变量配置

--- mysql/connection.js
+++ mysql/connection.js
@@ -2,7 +2,7 @@
  * @Author: PengChaoQun 1152684231@qq.com
  * @Date: 2023-12-28 14:19:02
  * @LastEditors: PengChaoQun 1152684231@qq.com
- * @LastEditTime: 2024-02-07 20:59:24
+ * @LastEditTime: 2024-02-08 20:17:43
  * @FilePath: /experience-book-server/mysql/connection.js
  * @Description:
  */
@@ -10,17 +10,18 @@ const mysql = require('mysql2');
 // const { MYSQL_CONF } = require('../config/db');
 
 const MYSQL_CONF = {
-  host: 'localhost',
+  host: process.env.ENV == 'develop' ? 'localhost' : 'exp-book-mysql',
   user: 'root',
   password: '123456789',
   port: '3306',
   database: 'experience_book'
 };
 
+console.log(`ENV`, process.env.ENV);
 
 console.log(`数据库开始连接`);
 
-let db= mysql.createConnection(MYSQL_CONF);
+let db = mysql.createConnection(MYSQL_CONF);
 
 db.connect(function (err) {
   if (err) {
