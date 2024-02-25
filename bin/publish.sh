66318c0f3a6313b15a416a91423ca6ae034f4783
PengChaoQun <1152684231@qq.com>
2024-02-25 16:16:41 +0800

    ✨ feat: 修改命令行

--- bin/publish.sh
+++ bin/publish.sh
@@ -2,7 +2,19 @@
  # @Author: PengChaoQun 1152684231@qq.com
  # @Date: 2024-02-22 09:39:25
  # @LastEditors: PengChaoQun 1152684231@qq.com
- # @LastEditTime: 2024-02-22 11:39:40
+ # @LastEditTime: 2024-02-25 16:14:12
  # @FilePath: /experience-book-server/bin/publish.sh
  # @Description: 
 ### 
+
+cp -R ./bin /Users/pengchaoqun/code/experience-book/experience-book-host/server/
+cp -R ./http /Users/pengchaoqun/code/experience-book/experience-book-host/server/
+cp -R ./mysql /Users/pengchaoqun/code/experience-book/experience-book-host/server/
+cp -R ./node_modules /Users/pengchaoqun/code/experience-book/experience-book-host/server/
+cp -R ./routes /Users/pengchaoqun/code/experience-book/experience-book-host/server/
+cp -R ./uploads /Users/pengchaoqun/code/experience-book/experience-book-host/server/
+cp -R ./utils /Users/pengchaoqun/code/experience-book/experience-book-host/server/
+cp -R ./views /Users/pengchaoqun/code/experience-book/experience-book-host/server/
+cp -R ./app.js /Users/pengchaoqun/code/experience-book/experience-book-host/server/
+cp -R ./package.json /Users/pengchaoqun/code/experience-book/experience-book-host/server/
+cp -R ./package-lock.json /Users/pengchaoqun/code/experience-book/experience-book-host/server/
